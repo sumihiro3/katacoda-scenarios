074f321c1f07ebc4c4aebdbe88484ea7769ab411
sumihiro3 <sumihiro@gmail.com>
2020-02-27 18:54:29 +0900

    add clone script

---
+++ LineBotBasicCourse/LineBotBasicScenario/intoro_code.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# clone hands-on code
+git clone https://github.com/sumihiro3/line-bot-hands-on-vol1.git example
+# install dependent modules
+cd example
+npm install
+# clear
+# Done!!
+echo "ソースコードのインストールが完了しました！"
