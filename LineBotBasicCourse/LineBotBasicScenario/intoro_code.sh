4803b414600a5d8453572a7fc917ed27307f7a5e
sumihiro3 <sumihiro@gmail.com>
2020-02-27 19:06:37 +0900

    install node.js using n

--- LineBotBasicCourse/LineBotBasicScenario/intoro_code.sh
+++ LineBotBasicCourse/LineBotBasicScenario/intoro_code.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# install node.js using n
+sudo npm install -g n
+# change node v12.15.0
+sudo n 12.16.1
 # clone hands-on code
 git clone https://github.com/sumihiro3/line-bot-hands-on-vol1.git example
 # install dependent modules
