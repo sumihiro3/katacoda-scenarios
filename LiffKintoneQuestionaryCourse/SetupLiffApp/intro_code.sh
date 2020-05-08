17dce949c1cf3522221cd839c55f2da217e42af5
sumihiro3 <sumihiro@gmail.com>
2020-05-08 10:53:38 +0900

    remove unnecessary commands

--- LiffKintoneQuestionaryCourse/SetupLiffApp/intro_code.sh
+++ LiffKintoneQuestionaryCourse/SetupLiffApp/intro_code.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-# install node.js using n
-# sudo npm install -g n
 # download and install heroku cli
 curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
-# # change node v12.16.3
-# sudo n 12.16.3
 # clone hands-on code
-git clone https://github.com/sumihiro3/liff-kintone-questionary.git example
-# install dependent modules
-cd example
-# npm install
+git clone https://github.com/sumihiro3/liff-kintone-questionary.git app
+# move to project directory
+cd app
 clear
 # Done!!
 echo "インストールが完了しました！"
